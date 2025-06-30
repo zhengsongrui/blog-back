9d9681ca19c181a350288c6ba451174abd45f6c9
zhengsongrui <1245881972@qq.com>
2025-06-30 08:43:59 +0800

    去除console

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,5 @@
 require('dotenv').config();
 
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-})
 module.exports = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
